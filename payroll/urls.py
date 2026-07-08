b6f498ca52e1a73f5d3836fad8e88ab4c791d597
LAB22_USER ON WS14 <L22X13W14@LabHCI.net>
2026-07-08 12:31:52 +0800

    Updated url.py

--- payroll/urls.py
+++ payroll/urls.py
@@ -5,6 +5,14 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+
+    # Admin Workspace View Routings
     path('dashboard/admin/', views.admin_dashboard, name='admin_dashboard'),
+    path('dashboard/admin/employees/', views.manage_employees, name='manage_employees'),
+    path('dashboard/admin/computation/', views.payroll_computation, name='payroll_computation'),
+    path('dashboard/admin/payslips/', views.admin_payslips, name='admin_payslips'),
+    path('dashboard/admin/reports/', views.admin_reports, name='admin_reports'),
+
+    # Employee Workspace View Routings
     path('dashboard/staff/', views.staff_dashboard, name='staff_dashboard'),
 ]
