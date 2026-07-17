20a9fc8ed0ce9ce979a61f2b0d6280d772d65dfb
LAB22_USER ON WS14 <L22X13W14@LabHCI.net>
2026-07-17 14:51:21 +0800

    Fixed static error

--- payme/settings.py
+++ payme/settings.py
@@ -71,7 +71,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'payroll' / 'static',
+    BASE_DIR / 'static',
 ]
 
 STORAGES = {
