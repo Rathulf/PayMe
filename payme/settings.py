078a0960d88dde448f86f22d309ba4d5edfdf99c
rathulf <ralphsampan@gmail.com>
2026-07-18 23:04:39 +0800

    Fixed routing error on base_auth.html

--- payme/settings.py
+++ payme/settings.py
@@ -71,7 +71,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'payme' / 'static',
 ]
 
 STORAGES = {
